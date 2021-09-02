fe66f570b2c53b4d01ad6c4fce3ef66b87f7c75d
psimanta <simanta.paul.26@gmail.com>
2021-09-03 04:08:02 +0600

    Category wise rendering

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -10,7 +10,12 @@ const Orders = () => {
     const [selectedCategory, setSelectedCategory] = useState("All");
     const orders = [
         { orderNo: 123456, price: 4200, status: "Pending" },
-        { orderNo: 123453, price: 480, status: "Pending" }
+        { orderNo: 123453, price: 480, status: "Pending" },
+        { orderNo: 123455, price: 480, status: "Confirmed" },
+        { orderNo: 123453, price: 480, status: "Pending" },
+        { orderNo: 123453, price: 480, status: "Pending" },
+        { orderNo: 123453, price: 480, status: "Confirmed" },
+        { orderNo: 123454, price: 480, status: "Cancelled" }
     ]
 
     const setCategory = (name) => {
@@ -40,7 +45,9 @@ const Orders = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {orders.map((item, i) => <OrderRow order={item} key={i} sl={i} />)}
+                        {selectedCategory === "All" ?
+                            orders.map((item, i) => (<OrderRow order={item} key={i} sl={i} />)) :
+                            orders.filter(item => item.status === selectedCategory).map((item, i) => (<OrderRow order={item} key={i} sl={i} />))}
                     </tbody>
                 </table>
             </div>
