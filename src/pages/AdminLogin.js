98c6223d559b297b4f5161dae83b34738744e4e3
psimanta <simanta.paul.26@gmail.com>
2021-09-05 11:54:27 +0600

    Login Design

--- src/pages/AdminLogin.js
+++ src/pages/AdminLogin.js
@@ -45,14 +45,26 @@ const AdminLogin = ({ history }) => {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="username">User ID</label>
-                <input name="username" value={username} required onChange={handleChange} />
-                <label htmlFor="password">Password</label>
-                <input name="password" value={password} required onChange={handleChange} />
-                <button type="submit">Sign in</button>
-            </form>
+        <div style={{ paddingTop: "130px", }} className="row">
+            <div className="col-sm-4 offset-sm-4">
+                <center><h4>Admin Panel</h4></center>
+                <form onSubmit={handleSubmit} className="form">
+                    <label htmlFor="username">User ID</label>
+                    <input name="username" value={username} required onChange={handleChange} className="form-control" />
+                    <label htmlFor="password">Password</label>
+                    <input name="password" value={password} required onChange={handleChange} className="form-control" />
+                    <center><button type="submit" className="submit-btn">Sign in</button>
+                    </center>
+                </form>
+                <div style={{ borderRadius: "8px", border: "solid 1px black", padding: "5px", marginTop: "10px" }}>
+                    <h6>Use following credentials:</h6>
+                    <h6>User ID</h6>
+                    <p>TEST2021</p>
+                    <h6>Password</h6>
+                    <p>EASY1234</p>
+                </div>
+            </div>
+
         </div>
     )
 }
--- src/pages/UserLogin.js
+++ src/pages/UserLogin.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { withRouter, Redirect } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import Modal from "../components/Modal";
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import { USER_ENDPOINT } from "../utils/apiUrl";
@@ -54,13 +54,18 @@ const UserLogin = ({ history }) => {
                 <br /> Successfully
             </Modal>
             ) : ""}
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="username">Number</label>
-                <input name="username" value={username} required onChange={handleChange} />
-                <label htmlFor="password">Password</label>
-                <input name="password" value={password} required onChange={handleChange} />
-                <button type="submit">Sign up</button>
-            </form>
+            <div className="row" style={{ paddingTop: "90px" }}>
+                <div className="col-sm-4 offset-sm-4">
+                    <form onSubmit={handleSubmit} className="form">
+                        <label htmlFor="username">Number</label>
+                        <input name="username" value={username} required onChange={handleChange} className="form-control" />
+                        <label htmlFor="password">Password</label>
+                        <input name="password" value={password} required onChange={handleChange} className="form-control" />
+                        <center><button type="submit" className="submit-btn">Sign up</button>
+                        </center>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
